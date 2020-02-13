d7755ebfca49bade54a36190ac6bb262a16c1bba
Richard Eigenmann <richard.eigenmann@gmail.com>
2020-02-13 21:25:36 +0100

    Fix windows

--- executablePath.cpp
+++ executablePath.cpp
@@ -1,6 +1,12 @@
 #include <array>
-#include <linux/limits.h>
-#include <unistd.h>
+
+#if defined(LINUX)
+    #include <linux/limits.h>
+    #include <unistd.h>
+#elif defined _MSC_VER
+    #include <../include/limits.h>
+    #include <windows.h>
+#endif
 
 #ifdef __has_include
 # if __has_include(<filesystem>)
@@ -20,22 +26,22 @@
  * @see https://en.sfml-dev.org/forums/index.php?topic=12416.msg86622#msg86622
  */
 auto my_executable_path() noexcept (false) -> std::string {
-        char buff[PATH_MAX + 1]; // NOLINT(cppcoreguidelines-avoid-c-arrays, hicpp-avoid-c-arrays, modernize-avoid-c-arrays, cppcoreguidelines-pro-bounds-array-to-pointer-decay)
-//#if defined(LINUX)
-        ssize_t len = ::readlink("/proc/self/exe", buff, sizeof(buff) - 1); // NOLINT(hicpp-no-array-decay, cppcoreguidelines-pro-bounds-array-to-pointer-decay)
+#ifdef __linux__
+    char buff[PATH_MAX + 1]; // NOLINT(cppcoreguidelines-avoid-c-arrays, hicpp-avoid-c-arrays, modernize-avoid-c-arrays, cppcoreguidelines-pro-bounds-array-to-pointer-decay)
+    ssize_t len = ::readlink("/proc/self/exe", buff, sizeof(buff) - 1); // NOLINT(hicpp-no-array-decay, cppcoreguidelines-pro-bounds-array-to-pointer-decay)
         if (len != -1) {
             buff[len] = '\0'; //NOLINT(cppcoreguidelines-pro-bounds-constant-array-index, hicpp-no-array-decay, cppcoreguidelines-pro-bounds-array-to-pointer-decay)
             return fs::path(buff).parent_path().string();
         } 
         throw std::runtime_error( "Failed to retrieve the executable path from /poroc/self/exe" );
-/* #elif defined(WINDOWS)
-        HMODULE module_handle = GetModuleHandle(nullptr);
-        GetModuleFileName(module_handle, buff, sizeof(buff));
-        return std::filesystem::path(buff).parent_path.string();
+#elif _WIN32
+    char buff[MAX_PATH + 1];
+    HMODULE module_handle = GetModuleHandle(nullptr);
+    GetModuleFileName(module_handle, buff, sizeof(buff));
+    return fs::path(buff).parent_path().string();
 #else
 #error "MISSING CODE :-( - See StackOverflow 1023306"
-*/
-//#endif
+#endif
 }
 
 
